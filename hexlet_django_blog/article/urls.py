5b21c2d4553de2a508ed8ab9322674c9f06a88a4
Ekaterina Mitiashkina <algrince@gmail.com>
2022-11-20 19:43:42 +0100

    add redirect

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,4 +5,5 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('<str:tags>/<int:article_id>/', views.article, name='article')
     ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 
@@ -6,5 +6,8 @@ from django.http import HttpResponse
 class IndexView(View):
 
     def get(self, request):
-        return HttpResponse('Hello')
+        return redirect('article', tags='python', article_id=42)
 
+
+def article(request, tags, article_id):
+    return HttpResponse(f'Статья номер {article_id}. Тег {tags}.')
